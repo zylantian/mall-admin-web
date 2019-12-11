c1813f790b7f5a592638f3b74c135f8472c4f7c0
zhangyang <zhangyang@appexnetworks.com>
2019-12-11 16:12:31 +0800

    去掉品牌信息，商品编辑时不显示商品分类的问题

--- src/views/pms/product/components/ProductInfoDetail.vue
+++ src/views/pms/product/components/ProductInfoDetail.vue
@@ -13,7 +13,7 @@
       <el-form-item label="副标题：" prop="subTitle">
         <el-input v-model="value.subTitle"></el-input>
       </el-form-item>
-      <el-form-item label="商品品牌：" prop="brandId">
+      <!--<el-form-item label="商品品牌：" prop="brandId">
         <el-select
           v-model="value.brandId"
           @change="handleBrandChange"
@@ -25,7 +25,7 @@
             :value="item.value">
           </el-option>
         </el-select>
-      </el-form-item>
+      </el-form-item>-->
       <el-form-item label="商品介绍：">
         <el-input
           :autoSize="true"
@@ -73,7 +73,7 @@
       value: Object,
       isEdit: {
         type: Boolean,
-        default: false
+        default: true
       }
     },
     data() {
@@ -90,7 +90,6 @@
           ],
           subTitle: [{required: true, message: '请输入商品副标题', trigger: 'blur'}],
           productCategoryId: [{required: true, message: '请选择商品分类', trigger: 'blur'}],
-          brandId: [{required: true, message: '请选择商品品牌', trigger: 'blur'}],
           description: [{required: true, message: '请输入商品介绍', trigger: 'blur'}],
           requiredProp: [{required: true, message: '该项为必填项', trigger: 'blur'}]
         }
@@ -98,7 +97,7 @@
     },
     created() {
       this.getProductCateList();
-      this.getBrandList();
+      // this.getBrandList();
     },
     computed:{
       //商品的编号
@@ -127,8 +126,9 @@
       //处理编辑逻辑
       handleEditCreated(){
         if(this.value.productCategoryId!=null){
-          this.selectProductCateValue.push(this.value.cateParentId);
-          this.selectProductCateValue.push(this.value.productCategoryId);
+          this.selectProductCateValue = [this.value.cateParentId,this.value.productCategoryId]
+          //this.selectProductCateValue.push(this.value.cateParentId);
+          //this.selectProductCateValue.push(this.value.productCategoryId);
         }
         this.hasEditCreated=true;
       },
